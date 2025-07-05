c68bf87bce9e3da3451751d858e8596239cce83a
Ryan <ryanshaunsmith@gmail.com>
2025-07-05 09:33:54 +0100

    update test to use port 5173 and add cypress test to CI pipeline

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,6 +1,6 @@
 describe("My First Test", () => {
   it("Gets, types and asserts", () => {
-    cy.visit("http://localhost:3000");
+    cy.visit("http://localhost:5173");
 
     cy.contains("Pup Timers!");
   });
