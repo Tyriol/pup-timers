68c5e07496cda7604c392a673b60fc11a817155e
Ryan <ryanshaunsmith@gmail.com>
2025-07-08 08:53:34 +0100

    create context for dogs

--- src/context/Context.tsx
+++ src/context/Context.tsx
@@ -1,11 +1,20 @@
 import { createContext } from "react";
-import type { Timer } from "../types/types";
+import type { Timer, Dog } from "../types/types";
 
 interface TimersContextValue {
   timersList: Timer[];
   addTimer: (timer: Timer) => void;
 }
 
+interface DogsContextValue {
+  dogList: Dog[];
+  addDog: (dog: Dog) => void;
+}
+
 export const TimersContext = createContext<TimersContextValue | undefined>(
+  undefined,
+);
+
+export const DogsContext = createContext<DogsContextValue | undefined>(
   undefined,
 );
