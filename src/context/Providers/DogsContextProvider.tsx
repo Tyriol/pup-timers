764295e11bcaf0d00c49c1b2e3d0a0dfe8334c20
Ryan <ryanshaunsmith@gmail.com>
2025-07-08 09:15:13 +0100

    add provider for dogs context

---
+++ src/context/Providers/DogsContextProvider.tsx
@@ -0,0 +1,22 @@
+import { useState, type ReactNode } from "react";
+import { DogsContext } from "../Context";
+import type { Dog } from "../../types/types";
+
+interface DogsProviderProps {
+  children: ReactNode;
+}
+
+export const TimersProvider = ({ children }: DogsProviderProps) => {
+  const [dogsList, setDogsList] = useState<Dog[]>([]);
+
+  const addDog = (newDog: Dog) => {
+    setDogsList((prevDogs) => [...prevDogs, newDog]);
+  };
+
+  const value = {
+    dogsList,
+    addDog,
+  };
+
+  return <DogsContext.Provider value={value}>{children}</DogsContext.Provider>;
+};
