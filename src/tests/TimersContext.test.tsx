6960df01c96b7fc7255d9e8b7c117c34165a8419
Ryan <ryanshaunsmith@gmail.com>
2025-07-08 08:50:24 +0100

    add initial test for Timers Context

---
+++ src/tests/TimersContext.test.tsx
@@ -0,0 +1,26 @@
+import { render, screen } from "@testing-library/react";
+import { useContext } from "react";
+import { TimersContext } from "../context/Context";
+import { TimersProvider } from "../context/Providers/TimersContextProvider";
+import type { Timer } from "../types/types";
+import { describe, it, expect } from "vitest";
+
+describe("Timers Context", () => {
+  it("Provides an empty timers array by default", () => {
+    const TestComponent = () => {
+      const { timersList } = useContext(TimersContext) as {
+        timersList: Timer[];
+      };
+
+      return <div data-testid="timers-count">{timersList.length}</div>;
+    };
+
+    render(
+      <TimersProvider>
+        <TestComponent />
+      </TimersProvider>,
+    );
+
+    expect(screen.getByTestId("timers-count").textContent).toBe("0");
+  });
+});
