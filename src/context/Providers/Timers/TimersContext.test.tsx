233f5773acc11284c71c8396c76822b48d501b36
Ryan <ryanshaunsmith@gmail.com>
2025-07-09 08:52:48 +0100

    import userEvent as a default export rather than a named export from @testing-library/user-event

--- src/context/Providers/Timers/TimersContext.test.tsx
+++ src/context/Providers/Timers/TimersContext.test.tsx
@@ -1,5 +1,5 @@
 import { render, screen } from "@testing-library/react";
-import { userEvent } from "@testing-library/user-event";
+import userEvent from "@testing-library/user-event";
 import { useContext } from "react";
 import { TimersContext } from "../../Context";
 import { TimersProvider } from "./TimersContextProvider";
