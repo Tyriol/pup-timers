57e46450ef7baa0c35ed8ed4b08c8e434dcb139a
Ryan <ryanshaunsmith@gmail.com>
2025-07-09 08:51:23 +0100

    update TimersContextValue and DogContextValue interfaces to include the update and delete methods

--- src/context/Context.ts
+++ src/context/Context.ts
@@ -4,11 +4,15 @@ import type { Timer, Dog } from "../types/types";
 interface TimersContextValue {
   timersList: Timer[];
   addTimer: (timer: Timer) => void;
+  updateTimer: (timer: Timer) => void;
+  deleteTimer: (id: string) => void;
 }
 
 interface DogsContextValue {
   dogsList: Dog[];
   addDog: (dog: Dog) => void;
+  updateDog: (dog: Dog) => void;
+  deleteDog: (id: string) => void;
 }
 
 export const TimersContext = createContext<TimersContextValue | undefined>(
