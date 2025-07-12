9cef89f50c15bbebb3361e2e10286ed26acfcce9
Ryan <ryanshaunsmith@gmail.com>
2025-07-12 20:26:27 +0100

    [WIP] update test for adding a dog to work with local storage - other tests failing

--- src/context/Providers/Dogs/DogsContext.test.tsx
+++ src/context/Providers/Dogs/DogsContext.test.tsx
@@ -1,13 +1,15 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, waitFor } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import { useContext } from "react";
+import { IDBFactory } from "fake-indexeddb";
 import { describe, it, expect } from "vitest";
 import { DogsContext } from "../../Context";
 import { DogsProvider } from "./DogsContextProvider";
-import type { Dog } from "../../../types/types";
+import type { Dog, NewDog } from "../../../types/types";
 
 describe("Dogs Context", () => {
   it("Provide an empty dogs array by default", () => {
+    indexedDB = new IDBFactory();
     const TestDogComponent = () => {
       const { dogsList } = useContext(DogsContext) as { dogsList: Dog[] };
 
@@ -25,15 +27,16 @@ describe("Dogs Context", () => {
     expect(dogCount).toBe("0");
   });
 
-  it("add a new dog to the dogs array", async () => {
+  it("adds a new dog to the dogs array", async () => {
+    indexedDB = new IDBFactory();
     const TestDogComponent = () => {
-      const { dogsList, addDog } = useContext(DogsContext) as {
-        dogsList: Dog[];
-        addDog: (newDog: Dog) => void;
+      const context = useContext(DogsContext);
+
+      const handleAddDog = () => {
+        context?.addDog(newDog);
       };
 
-      const newDog = {
-        id: "1",
+      const newDog: NewDog = {
         name: "Buddy",
         age: 3,
         breed: "Golden Retriever",
@@ -41,10 +44,10 @@ describe("Dogs Context", () => {
 
       return (
         <>
-          <button data-testid="add-dog-button" onClick={() => addDog(newDog)}>
+          <button data-testid="add-dog-button" onClick={handleAddDog}>
             Add new Dog
           </button>
-          <div data-testid="dogs-count">{dogsList.length}</div>
+          <div data-testid="dogs-count">{context?.dogsList.length}</div>
         </>
       );
     };
@@ -55,26 +58,30 @@ describe("Dogs Context", () => {
       </DogsProvider>,
     );
 
-    const dogCountBefore = screen.getByTestId("dogs-count").textContent;
-    expect(dogCountBefore).toBe("0");
+    await waitFor(() => {
+      const dogCountBefore = screen.getByTestId("dogs-count").textContent;
+      expect(dogCountBefore).toBe("0");
+    });
 
     const addDogButton = screen.getByTestId("add-dog-button");
     await userEvent.click(addDogButton);
 
-    const dogCountAfter = screen.getByTestId("dogs-count").textContent;
-    expect(dogCountAfter).toBe("1");
+    await waitFor(() => {
+      const dogCountAfter = screen.getByTestId("dogs-count").textContent;
+      expect(dogCountAfter).toBe("1");
+    });
   });
 
   it("updates an existing dog in the dogs array", async () => {
+    indexedDB = new IDBFactory();
     const TestDogComponent = () => {
       const { dogsList, addDog, updateDog } = useContext(DogsContext) as {
         dogsList: Dog[];
-        addDog: (newDog: Dog) => void;
+        addDog: (newDog: NewDog) => void;
         updateDog: (updatedDog: Dog) => void;
       };
 
       const newDog = {
-        id: "1",
         name: "Buddy",
         age: 3,
         breed: "Golden Retriever",
@@ -115,25 +122,29 @@ describe("Dogs Context", () => {
 
     await userEvent.click(addDogButton);
 
-    const dogAgeBefore = screen.getByTestId("dogs-age").textContent;
-    expect(dogAgeBefore).toBe("3");
+    await waitFor(() => {
+      const dogAgeBefore = screen.getByTestId("dogs-age").textContent;
+      expect(dogAgeBefore).toBe("3");
+    });
 
     await userEvent.click(updateDogButton);
 
-    const dogAgeAfter = screen.getByTestId("dogs-age").textContent;
-    expect(dogAgeAfter).toBe("4");
+    await waitFor(() => {
+      const dogAgeAfter = screen.getByTestId("dogs-age").textContent;
+      expect(dogAgeAfter).toBe("4");
+    });
   });
 
   it("deletes a dog from the dog array", async () => {
+    indexedDB = new IDBFactory();
     const TestDogComponent = () => {
       const { dogsList, addDog, deleteDog } = useContext(DogsContext) as {
         dogsList: Dog[];
-        addDog: (newDog: Dog) => void;
+        addDog: (newDog: NewDog) => void;
         deleteDog: (id: string) => void;
       };
 
       const newDog = {
-        id: "1",
         name: "Buddy",
         age: 3,
         breed: "Golden Retriever",
