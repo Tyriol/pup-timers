40c79778d9cdebf6e289dd454f3fb031d52bc730
Ryan <ryanshaunsmith@gmail.com>
2025-07-22 18:29:18 +0100

    update test to fire up fakeIndexedDb to allow tests to run without errors

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,14 @@
 import App from "./App";
+import { IDBFactory } from "fake-indexeddb";
 import { render, screen } from "@testing-library/react";
-import { describe, it, expect } from "vitest";
+import { describe, it, expect, beforeEach } from "vitest";
 import { DogsProvider } from "./context/Providers/Dogs/DogsContextProvider";
 
 describe("App", () => {
+  beforeEach(() => {
+    indexedDB = new IDBFactory();
+  });
+
   it("renders the title correctly", () => {
     render(
       <DogsProvider>
