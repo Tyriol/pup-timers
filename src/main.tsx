ee04b18d66514839d4325c760ffe989a33313ce7
Ryan <ryanshaunsmith@gmail.com>
2025-07-24 17:57:54 +0100

    wrap app in TimersProvider

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,16 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { DogsProvider } from "./context/Providers/Dogs/DogsContextProvider.tsx";
+import { TimersProvider } from "./context/Providers/Timers/TimersContextProvider.tsx";
 import "./index.css";
 import App from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <DogsProvider>
-      <App />
+      <TimersProvider>
+        <App />
+      </TimersProvider>
     </DogsProvider>
   </StrictMode>,
 );
