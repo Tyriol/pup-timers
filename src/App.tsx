d08f248b8f6d3b1c9c9a36facf7d7faa3919fa2c
Ryan <ryanshaunsmith@gmail.com>
2025-07-25 09:42:14 +0100

    add countdown component and simple test in app for visibility while working on it

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import "./App.css";
 import Stopwatch from "./components/Timers/Stopwatch/Stopwatch";
+import Countdown from "./components/Timers/Countdown/Countdown";
 import type { Timer } from "./types/types";
 
 function App() {
@@ -11,10 +12,20 @@ function App() {
     name: "Last Wee",
   };
 
+  const cdTimer: Timer = {
+    type: "countdown",
+    id: 1,
+    elapsed: 0,
+    duration: 2592000,
+    isRunning: false,
+    name: "Next Tick and Flea Tab",
+  };
+
   return (
     <div className="flex flex-col items-center justify-center">
       <h1 className="font-bold text-4x1 underline">Pup Timers!</h1>
       <Stopwatch timer={timer} />
+      <Countdown timer={cdTimer} />
     </div>
   );
 }
---
+++ src/components/Timers/Countdown/Countdown.tsx
@@ -0,0 +1,59 @@
+import { useEffect, useState } from "react";
+import { formatTime } from "../../../lib/timers";
+import type { Timer } from "../../../types/types";
+interface CountdownProps {
+  timer: Timer;
+}
+
+const Countdown = ({ timer }: CountdownProps) => {
+  const [timeRemaining, setTimeRemaining] = useState<number>(
+    timer.duration! - timer.elapsed,
+  );
+  const [elapsedSecs, setElapsedSecs] = useState<number>(timer.elapsed);
+  const [stateDays, setStateDays] = useState<string>("");
+  const [stateTime, setStateTime] = useState<string>("");
+  const [isRunning, setIsRunning] = useState<boolean>(timer.isRunning);
+
+  useEffect(() => {
+    if (isRunning) {
+      const interval = setInterval(() => {
+        setElapsedSecs((prev) => prev + 1);
+        setTimeRemaining((prev) => prev - 1);
+      }, 1000);
+      return () => clearInterval(interval);
+    }
+  }, [isRunning]);
+
+  useEffect(() => {
+    const { displayDays, displayTime } = formatTime(timeRemaining);
+    setStateDays(() => displayDays);
+    setStateTime(() => displayTime);
+  }, [elapsedSecs]);
+
+  const toggleTimerOnOff = () => {
+    setIsRunning((prev) => !prev);
+  };
+
+  const resetElapsedTime = () => {
+    setElapsedSecs(0);
+    setTimeRemaining(timer.duration!);
+  };
+
+  return (
+    <>
+      <div className="flex flex-col items-center justify-center gap-5 p-20">
+        <p>{timer.name}</p>
+        <p>{stateDays}</p>
+        <p>{stateTime}</p>
+        <button onClick={toggleTimerOnOff}>
+          {isRunning ? "Stop" : "Start"}
+        </button>
+        {!isRunning && elapsedSecs > 0 ? (
+          <button onClick={resetElapsedTime}>Reset</button>
+        ) : null}
+      </div>
+    </>
+  );
+};
+
+export default Countdown;
