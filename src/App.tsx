c777db5aac6f9687d97d906dcee586edcaeef1d9
Ryan <ryanshaunsmith@gmail.com>
2025-07-28 14:43:51 +0100

    use correct tailwind class for font-size and remove h1 styling in index.css

--- src/App.tsx
+++ src/App.tsx
@@ -23,7 +23,7 @@ function App() {
 
   return (
     <div className="flex flex-col items-center justify-center">
-      <h1 className="font-bold text-4x1 underline">Pup Timers!</h1>
+      <h1 className="font-bold text-4xl underline">Pup Timers!</h1>
       <Stopwatch timer={timer} />
       <Countdown timer={cdTimer} />
     </div>
