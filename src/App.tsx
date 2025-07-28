01091b71b3c9179023e70d413250fd5616eb18c1
Ryan <ryanshaunsmith@gmail.com>
2025-07-28 14:45:37 +0100

    remove test components

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,9 @@
 import "./App.css";
-import Stopwatch from "./components/Timers/Stopwatch/Stopwatch";
-import Countdown from "./components/Timers/Countdown/Countdown";
-import type { Timer } from "./types/types";
 
 function App() {
-  const timer: Timer = {
-    type: "stopwatch",
-    id: 1,
-    elapsed: 0,
-    isRunning: false,
-    name: "Last Wee",
-  };
-
-  const cdTimer: Timer = {
-    type: "countdown",
-    id: 1,
-    elapsed: 0,
-    duration: 2592000,
-    isRunning: false,
-    name: "Next Tick and Flea Tab",
-  };
-
   return (
     <div className="flex flex-col items-center justify-center">
       <h1 className="font-bold text-4xl underline">Pup Timers!</h1>
-      <Stopwatch timer={timer} />
-      <Countdown timer={cdTimer} />
     </div>
   );
 }
