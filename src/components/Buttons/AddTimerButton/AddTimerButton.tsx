3f47fd945a6507200ca429617bafb9e24efbcec2
Ryan <ryanshaunsmith@gmail.com>
2025-07-30 20:27:27 +0100

    add a button for adding a timer with static timer

---
+++ src/components/Buttons/AddTimerButton/AddTimerButton.tsx
@@ -0,0 +1,33 @@
+import { useContext } from "react";
+import type { NewTimer } from "../../../types/types";
+import { TimersContext } from "../../../context/Context";
+
+const AddTimerButton = () => {
+  const { addTimer } = useContext(TimersContext);
+
+  const newTimer: NewTimer = {
+    type: "stopwatch",
+    elapsed: 0,
+    isRunning: false,
+    startTime: undefined,
+    endTime: undefined,
+    name: "Next wee",
+    duration: 300,
+  };
+
+  const handleAddTimer = async () => {
+    try {
+      await addTimer(newTimer);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <button onClick={() => void handleAddTimer()} className="w-full">
+      +
+    </button>
+  );
+};
+
+export default AddTimerButton;
