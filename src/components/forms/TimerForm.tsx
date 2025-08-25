0e327495f02dd3d0b1e148e359676a95168d805b
Ryan <ryanshaunsmith@gmail.com>
2025-08-25 17:20:16 +0100

    flesh out timer form and add initial logic to add a timer

--- src/components/forms/TimerForm.tsx
+++ src/components/forms/TimerForm.tsx
@@ -1,16 +1,65 @@
-import React from "react";
+import React, { useState, useContext } from "react";
+import { TimersContext } from "../../context/Context";
 
 interface TimerFormProps {
   setIsAddingTimer: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
 const TimerForm = ({ setIsAddingTimer }: TimerFormProps) => {
+  const [timerType, setTimerType] = useState("stopwatch");
+  const { addTimer } = useContext(TimersContext);
+
+  const handleAddTimer = async (formData: FormData) => {
+    try {
+      const name = formData.get("timerName");
+      const type = formData.get("timerType");
+
+      if (
+        typeof name !== "string" ||
+        (type !== "stopwatch" && type !== "countdown")
+      ) {
+        throw new Error("Invalid form data");
+      }
+
+      await addTimer({ name, type });
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setIsAddingTimer(false);
+    }
+  };
+
   return (
-    <form>
+    <form className="flex flex-col" action={handleAddTimer}>
       <label>
-        <input type="text" />
+        Timer Name:
+        <input name="timerName" type="text" />
       </label>
-      <button onClick={() => setIsAddingTimer(false)}>Close Form</button>
+      <label>
+        StopWatch
+        <input
+          onClick={() => setTimerType("stopwatch")}
+          type="radio"
+          name="timerType"
+          value="stopwatch"
+        />
+      </label>
+      <label>
+        Countdown
+        <input
+          onClick={() => setTimerType("countdown")}
+          type="radio"
+          name="timerType"
+          value="countdown"
+        />
+      </label>
+      {timerType === "countdown" ? (
+        <label>
+          Duration:
+          <input name="duration" type="number" />
+        </label>
+      ) : null}
+      <button>Close Form</button>
     </form>
   );
 };
